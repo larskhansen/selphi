d1cd94301d8fdfcc3c90b47c47841d7ec0274359
lkh <lkh@mbp-egen-lkh.local>
2024-07-05 19:09:41 +0200

    Now the delete function is working

--- public/index.php
+++ public/index.php
@@ -22,14 +22,14 @@ require '../vendor/autoload.php';
 $selphi = new SelPhi();
 
 $folders = $selphi->getFolders();
-
-$name = $_COOKIE["name"] ?? "";
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    
+  $name = isset($_POST['name']) ? $_POST["name"] : (isset($_COOKIE["name"]) ? $_COOKIE["name"] : "");
+  
   $folder = getcwd() . $selphi->getUploadDir() . "/" .  str_replace(" ", "", strtolower($name));
-  if (empty($name) && is_dir($folder)) {
-    $_SESSION['uploadStatus'] = "Dette navn er allerede taget, brug et andet.";
+  if (is_dir($folder) && !isset($_COOKIE["name"])) {
+    $_SESSION['uploadStatus'][] = "Dette navn er taget, brug et andet";
   } else {
-    $name = isset($_POST['name']) ? $_POST["name"] : $name;
     if ($name !== "") {
       // Set cookie for the name input field and the images.
       setcookie("name", $name, time()+86400*5, "/", $_SERVER['SERVER_NAME'], false);
@@ -40,18 +40,24 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
       }
     }
   }
+} else if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["image"]) && isset($_GET["action"]) && $_GET["action"] === "delete" && isset($_COOKIE["name"])) {
+  $getArray = explode("/", $_GET["image"]);
+  if (isset($getArray[1]) && str_replace(" ", "", strtolower($_COOKIE["name"])) === $getArray[1]) {
+    unlink($_GET["image"]);
+    header("Location:/");
+  }
 }
 $loader = new FilesystemLoader('../src/template');
 $twig = new Environment($loader, ['debug' => true]);
 $twig->addExtension(new DebugExtension());
-$status = $_SESSION['uploadStatus'] ?? "maks ti filer ad gangen.";
-
+$statusText = $_SESSION['uploadStatus'] ?? "maks ti filer ad gangen.";
+$status = ($statusText !== "maks ti filer ad gangen.") ? "text-danger" : "text-success";
 echo $twig->render(
   'index.html', 
   [
-    'name' => $name,
-    'enablednamefield' => !($name !== ""),
-    'uploadStatus' => $status,
+    'enablednamefield' => !isset($_COOKIE["name"]),
+    'uploadStatus' => $statusText,
+    'status' => $status,
     'folders' => $folders,
   ]
 );
